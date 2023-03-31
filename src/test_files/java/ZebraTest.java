cf4492e03820a81f3ccc65a6773f269630481d3b
jane <janeeslevin@gmail.com>
2023-03-31 20:46:00 +0100

    Updating test code

--- src/test_files/java/ZebraTest.java
+++ src/test_files/java/ZebraTest.java
@@ -1,7 +1,8 @@
-package test.java;
+package test_files.java;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.junit.Test;
@@ -13,27 +14,47 @@ public class ZebraTest {
 
     @Test
     public void runTest() {
+
         ConstraintSolver solver = new ConstraintSolver();
 
-        // Actual answer
-        String formattedAnswer = solver.printAnswer("data.txt");
+        ArrayList<String> myAnswer = solver.printAnswer("data.txt");
+        ArrayList<String> expectedAnswer = new ArrayList<String>();
 
-        // Expected answer
         File inputFile = new File("sol.txt");
         try (Scanner scanner = new Scanner(inputFile)) {
-            String expectedAnswer = "";
 
                 while (scanner.hasNextLine()) {
                     String currentLine = scanner.nextLine();
-                    expectedAnswer += currentLine += "\n";
+                    expectedAnswer.add(currentLine);
                 }
-
-            // Compare
-            Assert.assertEquals(formattedAnswer, expectedAnswer);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } 
+
+        Boolean allFound = true;
+
+        if (myAnswer.size() != expectedAnswer.size()) {
+            allFound = false;
+        } else {
+            for (String str1 : expectedAnswer) {
+                str1 = str1.replaceAll(" ", "");
+                boolean found = false;
+                for (String str2 : myAnswer) {
+                    str2 = str2.replaceAll(" ", "");
+                    if (str1.equals(str2)) {
+                        found = true;
+                        break;
+                    }
+                } 
+                if (!found) {
+                    allFound = false;
+                    break;
+                }
+            }
         }
+
+        Assert.assertTrue("Answers match", allFound == true);
 
     }
     
