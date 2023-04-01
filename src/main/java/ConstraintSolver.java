09206ab88e06f3199905f9e41c39c7dcb4c816ff
Rishi R. Kataria <82657139+Rishiry@users.noreply.github.com>
2023-04-01 15:38:42 +0100

    Update ConstraintSolver.java

--- src/main/java/ConstraintSolver.java
+++ src/main/java/ConstraintSolver.java
@@ -24,6 +24,10 @@ public class ConstraintSolver {
         //print variable
         for(int i = 0; i < variableSet.size(); i++)
             System.out.println(variableSet.get(i));
+        System.out.println("");
+        //print constraints
+        for(int i = 0; i < constraintSet.size(); i++)
+            System.out.println(constraintSet.get(i));
         return "";
         //TODO
         //needs to print constraints as well
@@ -54,6 +58,26 @@ public class ConstraintSolver {
                 } else if (currentLine.startsWith("Cons-")) {
                     //TODO
                     //this is the code for the constraints
+                    //ConstraintEqualityVarPlusCons:
+                    if (currentLine.startsWith("Cons-eqVPC")) {
+                        String regexPattern = "\\(|\\)|\\ ";
+                        String s = currentLine.replace("Cons-eqVPC(","").replaceAll(regexPattern, "");
+                        String[] values = s.split("="); 
+                        String[] values2 = values[1].split("\\+");
+                        String val1Name = values[0];
+                        String val2Name = values2[0];
+                        Variable v1 = null;
+                        Variable v2 = null;
+                        for (Variable element : variableSet) {
+                            if (element.hasThisName(val1Name)) {
+                                v1 = element;
+                            } else if(element.hasThisName(val2Name)) {
+                                v2 = element; 
+                            }
+                        }
+                        ConstraintEqualityVarPlusCons eq = new ConstraintEqualityVarPlusCons(v1, v2, Integer.parseInt(values2[1]), false);
+                        constraintSet.add(eq);
+                    }
                 }
 
             }
@@ -66,9 +90,16 @@ public class ConstraintSolver {
         }
     }
 
+    public void reduce() {
+        for(int i = 0; i < constraintSet.size(); i++)
+            constraintSet.get(i).reduce(); 
+    }
+
     public static void main(String[] args) {
         ConstraintSolver problem = new ConstraintSolver();
         problem.parse("data.txt");
+        System.out.println(problem);
+        problem.reduce();
         System.out.println(problem);
     }
 
